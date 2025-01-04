8c03182436004ac66a3902a966448d28e2e09f81
web-source-dev <muhammadnouman72321@gmail.com>
2025-01-04 19:47:31 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
